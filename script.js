32b457372b1a81192e7f108d310d0273a394de17
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-11-03 16:24:41 -0700

    Added function that creates div elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const board = document.querySelector('#board')
+
+function createDivs(row,col) {
+  for(let i = 0; i < (row * col); i++) {
+    const div = document.createElement('div');
+    board.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    board.appendChild(div).classList.add('box');
+  }
+}
+createDivs(30,30)
