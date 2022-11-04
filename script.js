dd12fce73379f4a99f9164c57ce89c60d165af9b
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-11-04 14:18:06 -0700

    Created the hovering function and added an eventlistener

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+//Global variables
 const board = document.querySelector('#board');
+let color = 'black';
 
-//Creates div elements 
+
+//Creates div elements and has hover feature
 function createDivs(row,col) {
   for(let i = 0; i < (row * col); i++) {
     const div = document.createElement('div');
+    div.addEventListener('mouseover', colorSquare);
     board.style.gridTemplateRows = `repeat(${row}, 1fr)`;
     board.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
     board.appendChild(div).classList.add('box');
   }
 }
 createDivs(30,30)
+
+//changes the color of each cell
+function colorSquare() {
+  this.style.backgroundColor = color;
+}
