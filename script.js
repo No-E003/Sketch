c67b657cab6581b9cb27185cf6810332802f2326
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-11-04 15:24:05 -0700

    Created a function that changes variable to a different color depending on what is clicked

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let color = 'black';
 function createDivs(row,col) {
   for(let i = 0; i < (row * col); i++) {
     const div = document.createElement('div');
-    div.addEventListener('mouseover', colorSquare);
+    div.addEventListener('mouseover', colorDiv);
     board.style.gridTemplateRows = `repeat(${row}, 1fr)`;
     board.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
     board.appendChild(div).classList.add('box');
@@ -16,6 +16,12 @@ function createDivs(row,col) {
 createDivs(30,30)
 
 //changes the color of each cell
-function colorSquare() {
+function colorDiv() {
   this.style.backgroundColor = color;
 }
+//changes the color
+function colorChoice(choice) {
+  color = choice;
+}
+
+//    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%);`
